69e3d1e9d32949b6681fd803fbe1c2fa9ce2343d
Raekye <adrian@creatifcubed.com>
2015-02-21 19:30:21 -0500

    implemented full stat

--- Query.lua
+++ Query.lua
@@ -1,6 +1,10 @@
 local PACKET_TYPE_HANDSHAKE = 9
 local PACKET_TYPE_STAT = 0
 
+local PACKET_MAGIC = { 0xFE, 0xFD }
+local PACKET_STAT_PADDING_1 = { 0x73, 0x70, 0x6C, 0x69, 0x74, 0x6E, 0x75, 0x6D, 0x00, 0x80, 0x00 }
+local PACKET_STAT_PADDING_2 = { 0x01, 0x70, 0x6C, 0x61, 0x79, 0x65, 0x72, 0x5F, 0x00, 0x00 }
+
 local CHALLENGE_TOKEN = os.time()
 
 local g_UDPSocket = nil
@@ -80,7 +84,7 @@ end
 
 function HandlePacketHandshake(a_Data, a_Host, a_Port)
 	local SessionId = PacketReadInt(a_Data:sub(4))
-	local Data = PacketCreate(PACKET_TYPE_HANDSHAKE, SessionId, tostring(CHALLENGE_TOKEN) .. string.char(0))
+	local Data = PacketCreate(PACKET_TYPE_HANDSHAKE, SessionId, tostring(CHALLENGE_TOKEN) .. "\0")
 	UDPSend(Data, a_Host, a_Port)
 end
 
@@ -95,23 +99,88 @@ function HandlePacketStat(a_Data, a_Host, a_Port)
 		return
 	end
 
-	local Server = cRoot:Get():GetServer()
+	if a_Data:len() == 11 then
+		HandleBasicStat(SessionId, a_Host, a_Port)
+	elseif a_Data:len() == 15 then
+		HandleFullStat(SessionId, a_Host, a_Port)
+	end
+end
 
-	local MOTD = Server:GetDescription()
-	-- gametype is hardcoded to SMP as per http://wiki.vg/Query
-	local GameType = "SMP"
-	local Map = cRoot:Get():GetDefaultWorld():GetName()
-	local NumPlayers = tostring(Server:GetNumPlayers())
-	local MaxPlayers = tostring(Server:GetMaxPlayers())
-	local HostPort = g_IniFile:GetValue("Server", "Ports")
-	-- from MCServer source, file src/OSSupport/ServerHandleImpl.cpp
-	-- in cServerHandleImpl::Listen(UInt16 a_Port)
-	-- `memset(&name, 0, sizeof(name))` binds socket to all interfaces
-	local HostIp = "0.0.0.0"
-
-	local Message = { MOTD, GameType, Map, NumPlayers, MaxPlayers, HostPort, HostIp }
-	local Data = PacketCreate(PACKET_TYPE_STAT, SessionId, table.concat(Message, "\0") .. string.char(0))
+function HandleBasicStat(a_SessionId, a_Host, a_Port)
+	local Stat = StatData()
+	local Message =
+	{
+		Stat["hostname"],
+		Stat["gametype"],
+		Stat["map"],
+		Stat["numplayers"],
+		Stat["maxplayers"],
+		Stat["hostport"],
+		Stat["hostip"],
+	}
+	local Data = PacketCreate(PACKET_TYPE_STAT, a_SessionId, table.concat(Message, "\0") .. "\0")
 	UDPSend(Data, a_Host, a_Port)
+end
+
+function HandleFullStat(a_SessionId, a_Host, a_Port)
+	local Players = {}
+	cRoot:Get():ForEachPlayer(function (a_Player)
+		if #Players < 16 then
+			Players[#Players + 1] = a_Player:GetName()
+		end
+	end)
+
+	local Message = string.char(unpack(PACKET_STAT_PADDING_1))
+
+	for k, v in pairs(StatData()) do
+		Message = Message .. string.format("%s\0%s\0", k, v)
+	end
+	Message = Message .. "\0"
+
+	Message = Message .. string.char(unpack(PACKET_STAT_PADDING_2))
+
+	Message = Message .. table.concat(Players, "\0") .. "\0\0"
+
+	local Data = PacketCreate(PACKET_TYPE_STAT, a_SessionId, Message)
+	UDPSend(Data, a_Host, a_Port)
+end
+
+function StatData()
+	local Server = cRoot:Get():GetServer()
+	local PluginManager = cRoot:Get():GetPluginManager()
+
+	-- currently hardcoded
+	local Version = "1.8"
+	local Plugins = {}
+	for k, v in pairs(PluginManager:GetAllPlugins()) do
+		if v ~= false then
+			Plugins[#Plugins + 1] = string.format("%s v%s", k, tostring(v:GetVersion()))
+		end
+	end
+
+	-- these keys are the ones in the key-value section of a full stat as listed in http://wiki.vg/Query
+	return {
+		-- actually the motd, but called hostname for some reason
+		hostname = Server:GetDescription(),
+
+		-- hardcoded as per http://wiki.vg/Query
+		gametype = "SMP",
+
+		-- hardcoded as per http://wiki.vg/Query
+		game_id = "MINECRAFT",
+
+		version = Version,
+		plugins = string.format("MCServer %s: %s", Version, table.concat(Plugins, "; ")),
+		map = cRoot:Get():GetDefaultWorld():GetName(),
+		numplayers = tostring(Server:GetNumPlayers()),
+		maxplayers = tostring(Server:GetMaxPlayers()),
+		hostport = g_IniFile:GetValue("Server", "Ports"),
+
+		-- from MCServer source, file src/OSSupport/ServerHandleImpl.cpp
+		-- in cServerHandleImpl::Listen(UInt16 a_Port)
+		-- `memset(&name, 0, sizeof(name))` binds socket to all interfaces
+		hostip = "0.0.0.0",
+	}
 end
 
 function PacketCreate(a_PacketType, a_SessionId, a_Message)
@@ -119,7 +188,7 @@ function PacketCreate(a_PacketType, a_SessionId, a_Message)
 end
 
 function PacketHasMagic(a_Data)
-	return a_Data:byte(1) == tonumber("fe", 16) and a_Data:byte(2) == tonumber("fd", 16)
+	return a_Data:byte(1) == PACKET_MAGIC[1] and a_Data:byte(2) == PACKET_MAGIC[2]
 end
 
 function PacketReadInt(a_Data)
